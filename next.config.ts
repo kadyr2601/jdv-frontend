0b3917a2c976cece928d5064165e2e95f743bae6
kadyr2601 <2601kadyr@gmail.com>
2025-05-02 17:18:39 +0400

    change urls

--- next.config.ts
+++ next.config.ts
@@ -4,22 +4,23 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-                port: '8000',
-                pathname: '/media/**',
-
-                // protocol: 'https',
-                // hostname: 'jdv.ae',
-                // port: '',
+                // protocol: 'http',
+                // hostname: '127.0.0.1',
+                // port: '8000',
                 // pathname: '/media/**',
+
+                protocol: 'https',
+                hostname: 'site-preview.site',
+                port: '',
+                pathname: '/media/**',
             },
         ],
     },
     env: {
-        API_URL: "http://127.0.0.1:8000",
-        HostName: "http://localhost:3000",
-        // API_URL: "https://jdv.ae",
+        // API_URL: "http://127.0.0.1:8000",
+        // HostName: "http://localhost:3000",
+        HostName: "https://site-preview.site",
+        API_URL: "https://site-preview.site",
     },
 };
 
